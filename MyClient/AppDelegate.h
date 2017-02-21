0c507d8abbf55ce9cfcb77685d4e8481ddaf5a57
岳琛 <yuechen@kmf.com>
2017-02-21 15:41:18 +0800

    someCode

---
+++ MyClient/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  MyClient
+//
+//  Created by 岳琛 on 2017/2/21.
+//  Copyright © 2017年 Conny. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyClient/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  MyClient
+//
+//  Created by 岳琛 on 2017/2/21.
+//  Copyright © 2017年 Conny. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
